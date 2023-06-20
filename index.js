760cb1ccdb97e239f1e4502b0ef89a98a4fdecd7
Aarthy <aarthyvasubhav003@gmail.com>
2023-06-20 13:30:51 +0530

    change errors

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ mongoose.set('strictQuery', false);
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log('MongoDB Connected: ${conn.connection.host});
+    console.log('MongoDB Connected: ${conn.connection.host}');
   } catch (e) {
     console.log(e);
     process.exit(1);
